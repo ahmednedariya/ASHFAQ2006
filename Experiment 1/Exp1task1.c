895fff4b9d14e88b0ad60056c1d145cbb562d17d
ahmednedariya <ahmed9@eng.rizvi.edu.in>
2024-12-10 08:18:42 +0530

    Add files via upload

---
+++ Experiment 1/Exp1task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ printf("Full Name\t:Ahmed Shahid Nedariya\n");
+printf("Age\t\t:18\n");
+printf("class\t\t:FE\n");
+printf("Division\t:B\n");
+printf("UIN\t\t:241M028");
+
+ return 0;
+}
